45069de0e32660fb8639d64dd56aab28d70cdd20
Bernie Ramirez <brami010@student.ucr.edu>
2016-01-24 14:44:20 -0800

    CamPivot
    CamPivot

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Debug.Log(player.transform.position);
+
+		Vector3 Playertemp = player.transform.position;
+		Vector3 camTemp = gameObject.transform.position;
+
+		Playertemp.y = camTemp.y;
+
+		float dist = Vector3.Distance(Playertemp,camTemp);
+		Debug.Log(dist);
+
+	}
+}
