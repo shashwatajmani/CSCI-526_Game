7542db7d774c348bf74f929f64f2455dfefaa2ea
Bernie Ramirez <brami010@student.ucr.edu>
2016-02-07 22:45:14 -0800

    Added simple AI + NavMesh

---
+++ Assets/Scripts/Enemy/FollowPlayerTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayerTest : MonoBehaviour {
+
+	NavMeshAgent agent;
+	public Transform followObject;
+
+	// Use this for initialization
+	void Start () {
+	
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(followObject)
+		{
+			agent.SetDestination(followObject.position);
+
+		}
+	
+	}
+}
---
+++ Assets/Scripts/Enemy/InRangeTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InRangeTest : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter( Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			Debug.Log("Player in Range");
+			GetComponentInParent<FollowPlayerTest>().followObject = other.gameObject.transform;
+		}
+	}
+
+
+}
