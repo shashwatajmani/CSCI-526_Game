a782bf1244d8064229a1ecf04c6e5cd9a2660e57
Bernie Ramirez <brami010@student.ucr.edu>
2016-02-09 17:07:06 -0800

    Added Level 1 Assets: Need to optimize!
    Level 1’s frame rate is really low!

---
+++ Assets/rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		gameObject.transform.Rotate (gameObject.transform.up*speed);
+	
+	}
+}
